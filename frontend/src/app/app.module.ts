eeac5caee0cfc6810142398e5e861a151bcfca49
Andreas Mattes <andi.mattes97@gmail.com>
2020-06-06 18:03:22 +0200

    nav & overview added, templates for todo items added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { OverviewComponent } from './components/overview/overview.component';
+import { TodoComponent } from './components/todo/todo.component';
+import { NavMobileComponent } from './components/nav-mobile/nav-mobile.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    OverviewComponent,
+    TodoComponent,
+    NavMobileComponent
   ],
   imports: [
     BrowserModule
