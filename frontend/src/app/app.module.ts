d89650e6b3ad20862698d9943a04c4cda6bf056e
Laura Legat <laura.legat11@gmail.com>
2020-06-12 00:25:57 +0200

    Basic routing added

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,9 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
+
+//define routes
+const appRoutes: Routes = [
+  {path: '', component: LoginComponent},
+  {path: 'register', component: RegisterComponent}, //localhost:4200/register
+  {path: 'login', component: LoginComponent }
+];
 
 @NgModule({
   declarations: [
@@ -12,7 +20,9 @@ import { RegisterComponent } from './components/register/register.component';
     RegisterComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    //register routes
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
