c6a69748d93294a46dbd4a346e7a9abdf59da4a5
leiti34 <leiti34@gmail.com>
2020-06-14 17:51:41 +0200

    added POST /board/:id to add a todo item

--- controller/src/board/board.controller.ts
+++ controller/src/board/board.controller.ts
@@ -12,38 +12,51 @@ import { JwtAuthGuard } from 'src/auth/jwt-auth.guard';
 import { BoardService } from './board.service';
 import { Board } from 'src/entity/board.entity';
 import { User } from 'src/entity/user.entity';
+import { TodoService } from 'src/todo/todo.service';
+import { Todo } from 'src/entity/todo.entity';
 
 @Controller('board')
 export class BoardController {
-  constructor(private readonly boardService: BoardService) {}
+  constructor(
+    private readonly boardService: BoardService,
+    private readonly todoService: TodoService,
+  ) {}
 
   @UseGuards(JwtAuthGuard)
   @Get()
-  async find(@Request() req): Promise<Board[]> {
+  async find(@Request() req: any): Promise<Board[]> {
     return await this.boardService.find(req.user as User);
   }
 
   @UseGuards(JwtAuthGuard)
   @Post()
-  async save(@Request() req: any): Promise<Board> {
+  async ave(@Request() req: any): Promise<Board> {
     return await this.boardService.save(req.user as User, req.body as Board);
   }
 
   @UseGuards(JwtAuthGuard)
   @Put()
-  async update(@Request() req): Promise<boolean> {
+  async update(@Request() req: any): Promise<boolean> {
     return await this.boardService.update(req.user as User, req.body as Board);
   }
 
   @UseGuards(JwtAuthGuard)
   @Delete()
-  async remove(@Request() req): Promise<boolean> {
+  async remove(@Request() req: any): Promise<boolean> {
     return await this.boardService.remove(req.user as User, req.body as Board);
   }
 
   @UseGuards(JwtAuthGuard)
   @Get(':id')
-  async findOne(@Param('id') id: number, @Request() req): Promise<Board> {
+  async findOne(@Param('id') id: number, @Request() req: any): Promise<Board> {
     return await this.boardService.findOne(req.user as User, id);
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post(':id')
+  async saveTodo(@Param('id') id: number, @Request() req: any): Promise<Todo> {
+    let todo: Todo = req.body as Todo;
+    //todo.board = { id: id } as Board;
+    return await this.todoService.save(req.user as User, req.body as Todo);
   }
 }
