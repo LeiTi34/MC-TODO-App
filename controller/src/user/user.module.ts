894e3d836757d0744b98892d74e0a50547917195
leiti34 <leiti34@gmail.com>
2020-06-24 15:33:42 +0200

    create default board on register

--- controller/src/user/user.module.ts
+++ controller/src/user/user.module.ts
@@ -3,11 +3,11 @@ import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 //import { Connection } from 'typeorm';
 import { User } from 'src/entity/user.entity';
+import { BoardService } from 'src/board/board.service';
 
 @Module({
   imports: [TypeOrmModule.forFeature([User])],
-  providers: [UserService],
+  providers: [UserService, BoardService],
   exports: [UserService],
 })
 export class UserModule {}
-
