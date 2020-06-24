7517c2fbee34b77a24bd1182818a2b6f170bb02c
leiti34 <leiti34@gmail.com>
2020-06-24 12:28:07 +0200

    fixed login

--- frontend/src/app/services/data.service.ts
+++ frontend/src/app/services/data.service.ts
@@ -47,7 +47,6 @@ export class DataService {
     this.token = await this.http
       .post<Token>(this.loginUrl, user, this.httpOptions)
       .toPromise();
-    //.subscribe((data) => (this.token = data));
 
     console.log(this.token);
 
@@ -57,11 +56,11 @@ export class DataService {
     return this.token != null;
   }
 
-  public register(user: User): boolean {
+  public async register(user: User): Promise<boolean> {
     console.log(user);
-    this.http
-      .post<Token>(this.loginUrl, user, this.httpOptions)
-      .subscribe((data) => (this.token = data));
+    this.token = await this.http
+      .post<Token>(this.registerUrl, user, this.httpOptions)
+      .toPromise();
 
     if (this.token != null) {
       this.router.navigate(["/boards"]);
@@ -137,16 +136,16 @@ export class DataService {
     }
   }
 
-  private handleError<T>(operation = "operation", result?: T) {
-    return (error: any): Observable<T> => {
-      // TODO: send the error to remote logging infrastructure
-      console.error(error); // log to console instead
+  //private handleError<T>(operation = "operation", result?: T) {
+  //return (error: any): Observable<T> => {
+  //// TODO: send the error to remote logging infrastructure
+  //console.error(error); // log to console instead
 
-      // TODO: better job of transforming error for user consumption
-      console.log(`${operation} failed: ${error.message}`);
+  //// TODO: better job of transforming error for user consumption
+  //console.log(`${operation} failed: ${error.message}`);
 
-      // Let the app keep running by returning an empty result.
-      return of(result as T);
-    };
-  }
+  //// Let the app keep running by returning an empty result.
+  //return of(result as T);
+  //};
+  //}
 }
